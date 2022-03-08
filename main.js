9e268a4fc96b6416f50f0eabed785889393a1023
Jasjeet393Singh <81762166+Jasjeet393Singh@users.noreply.github.com>
2022-03-08 21:42:25 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,3 @@
-
-
-
 var paddle2 =10,paddle1=10;
 
 var paddle1X = 10,paddle1Height = 110;
@@ -24,10 +21,12 @@ var ball = {
 rightWristY = 0;
 rightWristX = 0;
 scoreRightWrist = 0;
-
 game_status = "";
 
- 
+ function preload() {
+  ball_touch_paddel = loadSound("ball_touch_paddel.wav");
+  missed = loadSound("missed.wav");
+}
 
 function setup(){
 var canvas =  createCanvas(700,600);
@@ -59,13 +58,12 @@ function gotPoses(results)
 
 function startGame()
 {
-  game_status = "start"
-  document.getElementById("status").innerHTML = "Game is Loaded";
+  game_status = "start";
+  document.getElementById("status").innerHTML = "Game Is Loading";
 }
 
 function draw(){
-if(game_status == "start")
-{
+
   background(0); 
   image(video, 0, 0, 700, 600);
 
@@ -84,7 +82,9 @@ if(game_status == "start")
     circle(rightWristX, rightWristY, 30);
   }
 
-
+  if(game_status == "start")
+  {
+    document.getElementById("status").innerHTML = "Game Is Loaded";
     //funtion paddleInCanvas call 
     paddleInCanvas();
         
@@ -166,11 +166,11 @@ function move(){
   if (ball.x-2.5*ball.r/2< 0){
   if (ball.y >= paddle1Y&& ball.y <= paddle1Y + paddle1Height) {
     ball.dx = -ball.dx+0.5; 
-    
+    ball_touch_paddel.play();
   }
   else{
     pcscore++;
-    
+    missed.play();
     reset();
     navigator.vibrate(100);
   }
@@ -183,10 +183,10 @@ if(pcscore ==4){
     stroke("white");
     textSize(25);
     text("Game Over!",width/2,height/2);
-    text("Reload the page!",width/2,height/2+30)
+    text("Press Restart button to play again!",width/2,height/2+30)
     noLoop();
     pcscore = 0;
- }
+}
    if(ball.y+ball.r > height || ball.y-ball.r <0){
        ball.dy =- ball.dy;
    }   
@@ -206,13 +206,18 @@ function models(){
 
 //this function help to not go te paddle out of canvas
 function paddleInCanvas(){
-  if(paddle1Y+paddle1Height > height){
-    paddle1Y=height-paddle1Height;
+  if(mouseY+paddle1Height > height){
+    mouseY=height-paddle1Height;
   }
-  if(paddle1Y < 0){
-    paddle1Y =0;
+  if(mouseY < 0){
+    mouseY =0;
   }
  
   
 }
 
+function restart()
+{
+  pcscore = 0;
+  loop();
+}
